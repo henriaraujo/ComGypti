09949146c3f0af7d68843cf2d5c97ea41145194b
henriaraujo <henriarasilva@usp.br>
2019-10-19 14:39:05 -0300

    Adicionando modelos do Google API

--- core/models.py
+++ core/models.py
@@ -1,3 +1,8 @@
+import googlemaps
 from django.db import models
 
-# Create your models here.
+class Map:
+    gmaps = googlemaps.Client(key='AIzaSyAl_OhUmGOTt-c1Zt1y9ne0M8MXlc5Sotg')
+
+    def __str__(self):
+        return self.gmaps
